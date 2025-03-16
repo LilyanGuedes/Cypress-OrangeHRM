3fa3a232e0e777266e72d5543516a65bb3566013
LilyanGuedes <108057303+LilyanGuedes@users.noreply.github.com>
2025-03-16 16:54:22 -0300

    fix: informacoes do usuario sem nickName

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,13 +1,11 @@
 import userData from '../fixtures/users/userData.json'
+import { LoginPage } from '../pages/loginPage'
 
+const loginPage = new LoginPage()
 describe('Orange HRM Tests', () => {
 
   const selectorList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    wrongCredentialAlert: '.oxd-alert',
     dashboardGrid: '.orangehrm-dashboard-grid',
     myInfoButton:'[href="/web/index.php/pim/viewMyDetails"]',
     firstNameField: "[name='firstName']",
@@ -22,10 +20,9 @@ describe('Orange HRM Tests', () => {
   }
 
   it.only('User info update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSuccess.userName)
-    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorList.loginButton).click()
+
+    loginPage.accessLoginPage()
+    loginPage.loginwithUser(userData.userSuccess.userName, userData.userSuccess.password)
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     // cy.get(selectorList.sectionTitleTopBar).contains('Dashboard') não é bom usar textos
     cy.get(selectorList.dashboardGrid)
@@ -34,11 +31,11 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorList.firstNameField).clear().type("Maria")
     cy.get(selectorList.middleNameField).clear().type("Clara")
     cy.get(selectorList.lastNameField).clear().type("Santos")
-    cy.get(selectorList.genericField).eq(3).clear().type("NickNameTest")
-    cy.get(selectorList.genericField).eq(4).clear().type("employee")
-    cy.get(selectorList.genericField).eq(5).clear().type("1111")
-    cy.get(selectorList.genericField).eq(6).clear().type("2222")
-    cy.get(selectorList.genericField).eq(7).clear().type("2035-01-01")
+    // cy.get(selectorList.genericField).eq(3).clear().type("NickNameTest")
+    cy.get(selectorList.genericField).eq(3).clear().type("employee")
+    cy.get(selectorList.genericField).eq(4).clear().type("1111")
+    cy.get(selectorList.genericField).eq(5).clear().type("2222")
+    cy.get(selectorList.genericField).eq(6).clear().type("2035-01-01")
     cy.get(selectorList.dateCloseButton).click()
     cy.get(selectorList.genericCombobox).eq(0).click()
     cy.get(selectorList.fourthItemCombobox).click()
