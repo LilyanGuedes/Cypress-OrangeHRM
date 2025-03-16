5dcb1b6111a9ba5c6c8e71bcdd4e2a88b4fefed8
LilyanGuedes <108057303+LilyanGuedes@users.noreply.github.com>
2025-03-16 16:49:34 -0300

    feat: add login page objects

---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,22 @@
+export class LoginPage {
+  selectorList() {
+    const selectors = {
+      usernameField: "[name='username']",
+      passwordField: "[name='password']",
+      loginButton: "[type='submit']",
+      wrongCredentialAlert: '.oxd-alert',
+    }
+
+    return selectors
+  }
+
+  accessLoginPage() {
+    cy.visit('/auth/login')
+  }
+
+  loginwithUser(username, password) {
+    cy.get(this.selectorList().usernameField).type(username)
+    cy.get(this.selectorList().passwordField).type(password)
+    cy.get(this.selectorList().loginButton).click()
+  }
+}
